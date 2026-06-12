f03c7ae534bec72f29c885b3d9b75031876d5348
naziashakil27 <naziashakilnp@gmail.com>
2026-06-12 20:02:58 +0530

    chore: add gh-pages deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/diet-coke-tech-showcase/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
